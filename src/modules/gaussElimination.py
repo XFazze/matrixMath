95adf8b9bf080a735e1692531dcf5315c269cbc9
xfazze <a@fabbe90.gq>
2022-03-13 13:41:54 +0100

    restructuring

--- src/modules/gaussElimination.py
+++ src/modules/gaussElimination.py
@@ -1,63 +1,4 @@
 from copy import deepcopy
-import enum
-from os import O_WRONLY
-from exampleMatrixes import *
-
-
-def add(a, b):
-    if len(a) != len(b) and len(b) != 0:
-        return False
-    if len(a[0]) != len(b[0]) and len(b[0]) != 0:
-        return False
-    res = []
-    for i, x in enumerate(a):
-        t = []
-        for ii, y in enumerate(x):
-            t.append(y + b[i][ii])
-        res.append(t)
-
-    return res
-
-
-def k(A, k):
-    res = []
-    for i in A:
-        t = []
-        for ii in i:
-            t.append(ii*k)
-
-        res.append(t)
-    return res
-
-
-def multi(A, B):
-    if len(A) != len(B[0]):
-        return False
-    res = []
-    for rowA in A:
-        t = []
-        for columnI in range(len(B[0])):
-            x = 0
-            for i, num in enumerate(rowA):
-                x += num * B[i][columnI]
-            t.append(x)
-        res.append(t)
-
-    return res
-
-
-def t(A):
-    res = deepcopy(A)
-    for i in range(len(A)):
-        for ii in range(len(A[0])):
-            res[ii][i] = A[i][ii]
-
-    return res
-
-
-def p(A):
-    for i in A:
-        print(i)
 
 
 # column by column
@@ -130,9 +71,3 @@ def getZeros(A, index, B):
         A[rowI] = row
         B[rowI][0] -= amountMinus*B[index][0]
     return A
-
-
-gaussEleminering(C3x3, CC3x1)
-#p(add(k(A3x3, 3), k(B3x3, 2)))
-#p(multi(A3x3, B3x3))
-# p(t(A3x3))
